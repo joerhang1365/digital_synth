de9b0ca175a07787a9b9be7be16a5a8067d318b4
Joseph Hanger <48699911+joerhang1365@users.noreply.github.com>
2024-08-29 19:14:15 -0500

    fixed sine and square

--- main.c
+++ main.c
@@ -12,6 +12,9 @@
 #ifndef M_PI
 #define M_PI 3.14159265
 #endif
+#ifndef M_PI_2
+#define M_PI_2 M_PI * 2
+#endif
 #define NOTES_NUM 88
 
 enum wave_type { SINE, SQUARE, SAWTOOTH };
@@ -28,6 +31,42 @@ typedef struct
 wave_data;
 
 static double notes[NOTES_NUM];
+
+static void printSupportedStandardSampleRates(const PaStreamParameters *input_parameters,
+                                              const PaStreamParameters *output_parameters)
+{
+  static double standard_sample_rates[] = 
+  {
+    8000.0f, 9600.0f, 12000.0f, 16000.0f, 22050.0f, 24000.0f, 32000.0f,
+    44100.0f, 48000.0f, 88300.0f, 96000.0f, 192000.0f, -1 /* negative terminated list */
+  };
+
+  int print_count = 0;
+  PaError error;
+
+  for (int i = 0; standard_sample_rates[i] > 0; i++)
+  {
+    error = Pa_IsFormatSupported(input_parameters, output_parameters, standard_sample_rates[i]);
+    if (error == paFormatIsSupported)
+    {
+      if (print_count == 0)
+      {
+        printf("\t%8.2f", standard_sample_rates[i]);
+      }
+      else if (print_count == 4)
+      {
+        printf(",\n\t%8.2f", standard_sample_rates[i]);
+      }
+      else 
+      {
+        printf(", %8.2f", standard_sample_rates[i]);
+      }
+    }
+  }
+
+  if (!print_count) printf("none\n");
+  else printf("\n");
+}
 
 /* calculate hertz for each note
  * note A is notes[48] which is 440 hertz
@@ -61,25 +100,24 @@ static int paCallback(const void *inputBuffer, void *outputBuffer,
   {
     if (data->type == SINE)
     {
-      *(out++) = data->left_phase * data->amplitude; /* left */
-      *(out++) = data->right_phase * data->amplitude; /* right */
+      *(out++) = sin(data->left_phase) * data->amplitude; /* left */
+      *(out++) = sin(data->right_phase) * data->amplitude; /* right */
 
-      data->left_phase = sin(data->frequency * SAMPLE_RATE_INVERSE * i);
-      data->right_phase = sin(data->frequency * SAMPLE_RATE_INVERSE * i); 
+      data->left_phase += M_PI_2 * data->frequency * SAMPLE_RATE_INVERSE;
+      if (data->left_phase >= M_PI_2) data->left_phase -= M_PI_2;
+      data->right_phase += M_PI_2 * (data->frequency + data->phase_offset) * SAMPLE_RATE_INVERSE;
+      if (data->right_phase >= M_PI_2) data->right_phase -= M_PI_2;
     }
     else if (data->type == SQUARE)
     {
-      if (data->left_phase < 0.5f) *(out++) = -1.0f; /* left */
-      else *(out++) = 1.0f * data->amplitude;
+      *(out++) = sin(data->left_phase) >= 0 ? 1.0f * data->amplitude : -1.0f * data->amplitude;
+      *(out++) = sin(data->right_phase) >= 0 ? 1.0f * data->amplitude : -1.0f * data->amplitude;
 
-      if (data->right_phase < 0.5f) *(out++) = -1.0f; /* right */
-      else *(out++) = 1.0f * data->amplitude;
+      data->left_phase += M_PI_2 * data->frequency * SAMPLE_RATE_INVERSE;
+      if (data->left_phase >= M_PI_2) data->left_phase -= M_PI_2;
+      data->right_phase += M_PI_2 * (data->frequency + data->phase_offset) * SAMPLE_RATE_INVERSE;
+      if (data->right_phase >= M_PI_2) data->right_phase -= M_PI_2;
 
-      data->left_phase += data->frequency * SAMPLE_RATE_INVERSE;
-      if (data->left_phase >= 1.0f) data->left_phase -= 1.0f;
-
-      data->right_phase += data->frequency * SAMPLE_RATE_INVERSE + data->phase_offset;
-      if (data->right_phase >= 1.0f) data->right_phase = -1.0f;
     }
     else if (data->type == SAWTOOTH)
     {
@@ -88,11 +126,11 @@ static int paCallback(const void *inputBuffer, void *outputBuffer,
 
       data->left_phase += data->frequency * SAMPLE_RATE_INVERSE;
       if (data->left_phase >= 1.0f) data->left_phase -= 2.0f;
-      data->right_phase += data->frequency * SAMPLE_RATE_INVERSE + data->phase_offset;
+      data->right_phase += (data->frequency + data->phase_offset) * SAMPLE_RATE_INVERSE;
       if (data->right_phase >= 1.0f) data->right_phase -= 2.0f;
     }
     
-    printf("%f", *out);
+    printf("%f/n", *out);
   }
 
   return paContinue;
@@ -119,9 +157,12 @@ int main(int argc, char** argv)
 {
   PaStreamParameters output_parameters;
   PaStreamParameters input_parameters;
+  const PaDeviceInfo *device_info;
   PaStream *stream;
   PaError error;
   int user_device;
+  int num_devices;
+  int default_displayed;
   wave_data data;
 
   /* initialize PortAudio */
@@ -129,11 +170,10 @@ int main(int argc, char** argv)
   paCheckError(error);
 
   /* print PortAudio version */
-  printf("%s\n", Pa_GetVersionText());
+  printf("PortAudio version: 0x%08X\n", Pa_GetVersion());
+  printf("version text: '%s'\n", Pa_GetVersionInfo()->versionText);
 
   /* find audio device */
-  int num_devices;
-
   num_devices = Pa_GetDeviceCount();
   if (num_devices < 0)
   {
@@ -141,57 +181,98 @@ int main(int argc, char** argv)
     error = num_devices;
     paCheckError(error);
   }
-  else if (num_devices == 0)
-  {
-    printf("There are no devices connected\n");
-    endProgram(EXIT_SUCCESS);
-  }
-  else 
-  {
-    printf("Number of devices: %d\n", num_devices);
-  }
+  printf("number of devices: %d\n", num_devices);
 
-  const PaDeviceInfo *device_info;
   for (int i = 0; i < num_devices; i++)
   {
     device_info = Pa_GetDeviceInfo(i);
-    printf("device %d:\n", i);
-    printf("  name: %s:\n", device_info->name);
-    printf("  max input channels: %d\n", device_info->maxInputChannels);
-    printf("  max output channels: %d\n", device_info->maxOutputChannels);
-    printf("  default sample rate: %lf\n", device_info->defaultSampleRate);
+    printf("DEVICE %d: ----------------------------------------------\n", i);
+
+    /* mark global and API specific default devices */
+    default_displayed = 0;
+    if (i == Pa_GetDefaultInputDevice())
+    {
+      printf("[ default input");
+      default_displayed = 1;
+    }
+    else if(i == Pa_GetHostApiInfo(device_info->hostApi)->defaultInputDevice)
+    {
+      const PaHostApiInfo *host_info = Pa_GetHostApiInfo(device_info->hostApi);
+      printf("[ default %s input", host_info->name);
+      default_displayed = 1;
+    }
+    if (i == Pa_GetDefaultOutputDevice())
+    {
+      printf((default_displayed ? "," : "["));
+      printf(" default ouput");
+      default_displayed = 1;
+    }
+    else if(i == Pa_GetHostApiInfo(device_info->hostApi)->defaultOutputDevice)
+    {
+      const PaHostApiInfo *host_info = Pa_GetHostApiInfo(device_info->hostApi);
+      printf((default_displayed ? "," : "["));
+      printf(" default %s ouput", host_info->name);
+    }
+
+    if (default_displayed) printf(" ]\n");
+
+    /* print device info fields */
+    printf("name                          = %s\n", device_info->name);
+    printf("host API                      = %s\n", Pa_GetHostApiInfo(device_info->hostApi)->name);
+    printf("max input channels            = %d\n", device_info->maxInputChannels);
+    printf("max output channels           = %d\n", device_info->maxOutputChannels);
+    printf("default sample rate           = %8.2f\n", device_info->defaultSampleRate);
   }
 
-  printf("Select a device: ");
+  printf("select a device: ");
   int check = scanf("%d", &user_device);
 
   if (check != 1)
   {
-    printf("Incorrect device input\n");
+    printf("incorrect device input\n");
     endProgram(EXIT_SUCCESS);
   }
   else if (user_device > num_devices || user_device < 0)
   {
-    printf("Selected device does not exist\n");
+    printf("selected device does not exist\n");
     endProgram(EXIT_SUCCESS);
   }
 
+  device_info = Pa_GetDeviceInfo(user_device);
+
   /* set input and output parameters */
   memset(&input_parameters, 0, sizeof(input_parameters));
-  input_parameters.channelCount = CHANNEL_COUNT; // left and right
+  input_parameters.channelCount = device_info->maxInputChannels;
   input_parameters.device = user_device;
   input_parameters.hostApiSpecificStreamInfo = NULL;
   input_parameters.sampleFormat = paFloat32;
-  input_parameters.suggestedLatency = 
-    Pa_GetDeviceInfo(user_device)->defaultLowInputLatency;
+  input_parameters.suggestedLatency = device_info->defaultLowInputLatency;
 
   memset(&output_parameters, 0, sizeof(output_parameters));
-  output_parameters.channelCount = CHANNEL_COUNT; // left and right
+  output_parameters.channelCount = device_info->maxInputChannels; 
   output_parameters.device = user_device;
   output_parameters.hostApiSpecificStreamInfo = NULL;
   output_parameters.sampleFormat = paFloat32;
-  output_parameters.suggestedLatency = 
-    Pa_GetDeviceInfo(user_device)->defaultLowInputLatency;
+  output_parameters.suggestedLatency = device_info->defaultLowInputLatency;
+
+  if (input_parameters.channelCount > 0)
+  {
+    printf("supported standard sample rates\n for half-duplex 16 bit %d channel input = \n",
+           input_parameters.channelCount);
+    printSupportedStandardSampleRates(&input_parameters, NULL);
+  }
+  if (output_parameters.channelCount > 0)
+  {
+    printf("supported standard sample rates\n for half-duplex 16 bit %d channel output = \n",
+           output_parameters.channelCount);
+    printSupportedStandardSampleRates(NULL, &output_parameters);
+  }
+  if (input_parameters.channelCount > 0 && output_parameters.channelCount > 0)
+  {
+    printf("supported standard sample rates\n for half-duplex 16 bit %d channel input, %d channel output",
+           input_parameters.channelCount, output_parameters.channelCount);
+    printSupportedStandardSampleRates(&input_parameters, &output_parameters);
+  }
  
   /* open an audio I/O stream */
   error = Pa_OpenStream(&stream,
@@ -211,7 +292,7 @@ int main(int argc, char** argv)
   notesInit(); 
 
   /* initialize wave_data */
-  data.type = SINE;
+  data.type = SQUARE;
   data.frequency = notes[48]; /* A4 index 48*/
   data.amplitude = 1.0f;
   data.left_phase = 0.0f;
